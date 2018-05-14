73c4aada68fba0ce915c48aeffc82cf75f4c2b4a
Per Jansson <per.r.jansson@gmail.com>
2018-05-14 08:34:38 +0200

    unit and integration tests for start page

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -8,7 +8,12 @@ module.exports = {
         NODE_ENV: 'test',
     },
     moduleFileExtensions: ['js', 'jsx', 'json'],
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    },
     setupFiles: ['<rootDir>/jest.setup.js'],
     snapshotSerializers: ['enzyme-to-json/serializer'],
     testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-};
+}
---
+++ packages/web/src/start/__tests__/StartPage.test.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import 'jest-styled-components'
+import { MemoryRouter } from 'react-router-dom'
+
+import { Title, Rotate } from '@nordea-prep/components'
+
+import StartPage from '../StartPage'
+
+describe('<StartPage />', () => {
+    describe('unit test', () => {
+        it('should not rotate logo on initial state', () => {
+            const wrapper = shallow(<StartPage />)
+            expect(wrapper.find(Rotate).exists()).toBeFalsy()
+        })
+
+        it('should rotate logo when state isRotating true', () => {
+            const wrapper = shallow(<StartPage />)
+            wrapper.setState({ isRotating: true })
+            expect(wrapper.find(Rotate).exists()).toBeTruthy()
+        })
+    })
+    describe('integration test', () => {
+        it('should not rotate logo on initial state', () => {
+            const wrapper = mount(
+                <MemoryRouter>
+                    <StartPage />
+                </MemoryRouter>,
+            )
+            expect(wrapper.find(Rotate).exists()).toBeFalsy()
+        })
+
+        it('should rotate logo when Title is clicked', () => {
+            const wrapper = mount(
+                <MemoryRouter>
+                    <StartPage />
+                </MemoryRouter>,
+            )
+            wrapper.find(Title).simulate('click')
+            expect(wrapper.find(Rotate).exists()).toBeTruthy()
+        })
+
+        it('displays a link to the customers list page', () => {
+            const wrapper = mount(
+                <MemoryRouter>
+                    <StartPage />
+                </MemoryRouter>,
+            )
+            const link = wrapper.find('Link').find({ to: '/customers' })
+            expect(link.text()).toContain('customers list page')
+        })
+    })
+})
